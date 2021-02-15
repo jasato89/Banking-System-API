8ba82aeb503cdc9f64a63eb7de1bbebbbe34e9ca
Jaume <jasato89@gmail.com>
2021-02-15 08:58:12 +0100

    Deleted unused code

--- src/main/java/com/ironhack/bankingsystem/utils/PwdUtils.java
+++ src/main/java/com/ironhack/bankingsystem/utils/PwdUtils.java
@@ -14,17 +14,10 @@ import java.util.*;
 public class PwdUtils {
     public static void main(String[] args) throws JsonProcessingException {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        ObjectMapper objectMapper = new ObjectMapper();
-        CheckingAccountDTO checkingAccountDTO = new CheckingAccountDTO(new BigDecimal("200"), Currency.getInstance("USD"), "jasato", 1l, null);
 
-        System.out.println(objectMapper.writeValueAsString(new AccountHolderDTO("jasato123123", "123123",
-                "13-01-1989", "Jaume Sánchez", new Address("Calle Murillo","Madrid", "Sapin", 14, "05632"), new Address("Calle Murillo","Madrid", "Sapin", 14, "05632"))));
 
         System.out.println(passwordEncoder.encode("1234"));
-        int months = Period.between(LocalDate.of(2021, 02, 01), LocalDate.now()).getMonths();
-        long years = ChronoUnit.YEARS.between(LocalDate.of(1989, 12, 01), LocalDate.now());
-        System.out.println(months);
-        System.out.println(years);
+
 
     }
 }
